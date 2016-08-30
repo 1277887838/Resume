0ffeb29bccbad1720e524cd7f2224f5e8246bb62
asp.net <1277887838@qq.com>
2016-08-30 22:47:22 +0800

    1.0.2
    更新

--- .idea/index.js
+++ .idea/index.js
@@ -30,8 +30,14 @@
                     })
                     break;
                 case 3:
+                   // move('.page3 .bor').add('width', 100).duration('.7s').end();
+                    //move('.page3 h1').scale(0.8).end();
+                    move('.computer').set('margin-top','30px').duration('2s').end();
+                    move('.computer').set('opacity','1').duration('2s').end();
                     break;
                 case 4:
+                    move('.page4 .bor').add('width', 100).duration('.7s').end();
+                    move('.page4 h1').scale(0.8).end();
                     break;
             }
         },
@@ -55,10 +61,75 @@
                     });
                     break;
                 case 3:
+                   // move('.page3 .bor').sub('width', 100).end();
+                   // move('.page3 h1').scale(1).end();
+                    move('.computer').set('margin-top','50px').end();
+                    move('.computer').set('opacity','0').end();
                     break;
                 case 4:
+                    move('.page4 .bor').sub('width', 100).end();
+                    move('.page4 h1').scale(1).end();
                     break;
             }
         },
     })
 })
+
+//轮播
+var $index=0;
+var $exdex=0;
+$(".dh li").click(function(){
+    $index=$(this).index();
+    $(".dh li").eq($index).addClass("hover").siblings().
+    removeClass("hover");
+    if($index>$exdex){
+        next();
+    }else if($index<$exdex){
+        pre();
+    }
+    return $exdex=$index;
+});
+//下一页导航的点击事件
+$(".next").click(function(){
+    $index++;
+    if($index>3){
+        $index=0
+    }
+    $(".dh li").eq($index).addClass("hover").siblings().
+    removeClass("hover");
+    next();
+    return $exdex=$index;
+});
+//上一页导航的点击事件
+$(".pre").click(function(){
+    $index--;
+    if($index<0){
+        $index=3
+    };
+    $(".dh li").eq($index).addClass("hover").siblings().
+    removeClass("hover");
+    pre();
+    return $exdex=$index;
+});
+
+//定时器
+/*
+var atime=setInterval(function(){
+    $(".next").click();
+},3000);
+*/
+
+//下一页
+function next(){
+    $(".imgs li").eq($index).stop(true,true).
+    css("left","400px").animate({"left":"0"});
+    $(".imgs li").eq($exdex).stop(true,true).
+    css("left","0").animate({"left":"-400px"});
+}
+//上一页
+function pre(){
+    $(".imgs li").eq($index).stop(true,true).
+    css("left","400px").animate({"left":"0"});
+    $(".imgs li").eq($exdex).stop(true,true).
+    css("left","0").animate({"left":"400px"});
+}
