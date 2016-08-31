5afea389e0b759ec50cd2d94f6b57568a0ca1ead
Mata.chen <1277887838@qq.com>
2016-08-31 15:53:09 +0800

    1.0.3
    更新

--- .idea/index.js
+++ .idea/index.js
@@ -3,7 +3,8 @@
  */$(function () {
     $('#fullpage').fullpage({
         css3: true,
-        resize: false,
+        resize: true,
+        scrollOverflow:true,
         anchors: ['page1', 'page2', 'page3', 'page4'],
         navigation: true,
         navigationPosition: 'right',
@@ -30,14 +31,25 @@
                     })
                     break;
                 case 3:
-                   // move('.page3 .bor').add('width', 100).duration('.7s').end();
-                    //move('.page3 h1').scale(0.8).end();
+                    move('.page3 .bor').add('width', 100).duration('.7s').end();
+                    move('.page3 h1').scale(0.8).end();
                     move('.computer').set('margin-top','30px').duration('2s').end();
                     move('.computer').set('opacity','1').duration('2s').end();
                     break;
                 case 4:
                     move('.page4 .bor').add('width', 100).duration('.7s').end();
                     move('.page4 h1').scale(0.8).end();
+                    var i = -1;
+                    $(".lx p").each(function () {
+                        var $this = $(this);
+                        if (!$this.hasClass("t_to_t")) {
+                            i++;
+                            setTimeout(function () {
+                                $this.css("display","block");
+                                $this.addClass("t_to_t");
+                            }, 200 * i);
+                        }
+                    })
                     break;
             }
         },
@@ -61,14 +73,25 @@
                     });
                     break;
                 case 3:
-                   // move('.page3 .bor').sub('width', 100).end();
-                   // move('.page3 h1').scale(1).end();
+                    move('.page3 .bor').sub('width', 100).end();
+                    move('.page3 h1').scale(1).end();
                     move('.computer').set('margin-top','50px').end();
                     move('.computer').set('opacity','0').end();
                     break;
                 case 4:
                     move('.page4 .bor').sub('width', 100).end();
                     move('.page4 h1').scale(1).end();
+                    var i = -1;
+                    $(".lx p").each(function () {
+                        var $this = $(this);
+                        if ($this.hasClass("t_to_t")) {
+                            i++;
+                            setTimeout(function () {
+                                $this.css("display","none");
+                                $this.removeClass("t_to_t");
+                            }, 1);
+                        }
+                    })
                     break;
             }
         },
@@ -92,7 +115,7 @@ $(".dh li").click(function(){
 //下一页导航的点击事件
 $(".next").click(function(){
     $index++;
-    if($index>3){
+    if($index>2){
         $index=0
     }
     $(".dh li").eq($index).addClass("hover").siblings().
@@ -104,7 +127,7 @@ $(".next").click(function(){
 $(".pre").click(function(){
     $index--;
     if($index<0){
-        $index=3
+        $index=2
     };
     $(".dh li").eq($index).addClass("hover").siblings().
     removeClass("hover");
@@ -122,14 +145,14 @@ var atime=setInterval(function(){
 //下一页
 function next(){
     $(".imgs li").eq($index).stop(true,true).
-    css("left","400px").animate({"left":"0"});
+    css("left","765px").animate({"left":"0"});
     $(".imgs li").eq($exdex).stop(true,true).
-    css("left","0").animate({"left":"-400px"});
+    css("left","0").animate({"left":"-765px"});
 }
 //上一页
 function pre(){
     $(".imgs li").eq($index).stop(true,true).
-    css("left","400px").animate({"left":"0"});
+    css("left","765px").animate({"left":"0"});
     $(".imgs li").eq($exdex).stop(true,true).
-    css("left","0").animate({"left":"400px"});
+    css("left","0").animate({"left":"765px"});
 }
